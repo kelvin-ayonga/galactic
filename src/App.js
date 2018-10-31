5a1e358de438a03da7216657faea7cb73185c366
kelvin-ayonga <kelvin.ayonga@sunergix.com>
2018-10-31 18:01:12 +0300

    Creat an initial canvas, add spaceship and key events

--- src/App.js
+++ src/App.js
@@ -1,25 +1,114 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.image = new Image();
+    this.image.src =  "/ship.png";
+    const width = 100;
+    const height = 110;
+
+    this.state = {
+      height: window.innerHeight,
+      width: window.innerWidth,
+      map : {
+        37 : false,
+        38 : false,
+        39 : false,
+        40 : false,
+        32 : false
+      },
+      player : {
+        width: width,
+        height :height,
+        x : window.innerWidth / 2 - width / 2,
+        y : window.innerHeight  - ( height + 10),
+        power : 10
+      }
+    };
+
+    document.addEventListener("keydown", this._handleKeydownEvents, false);
+    document.addEventListener("keyup", this._handleKeyupEvents, false);
+  }
+
+  _handleKeyupEvents = (event) => {
+    if( event.keyCode in this.state.map) {
+      let map = {...this.state.map}
+      map[event.keyCode]  = false;
+      this.setState({map},() => {});
+    }
+  }
+
+  _handleKeydownEvents = (event) =>{
+    let step = 10;
+    if( event.keyCode in this.state.map) {
+      let map = {...this.state.map};
+      map[event.keyCode]  = true;
+
+      this.setState({map}, () => {
+        let player = {...this.state.player};
+        switch(event.keyCode){
+          case 37:
+            player.x += -step;
+            this.setState({player},()=> { });
+            break;
+          case 38:
+            player.y += -step;
+            this.setState({player},()=> { });
+            break;
+          case 39:
+            player.x += step;
+            this.setState({player},()=> { });
+            break;
+          case 40:
+            player.y += step;
+            this.setState({player},()=> { });
+            break;
+          case 32:
+            console.log("spacebar");
+            break;
+          default:
+            break;
+        }
+      });
+    }
+  }
+
+  componentDidMount() {
+    const canvas = this.refs.canvas
+    canvas.height = this.state.height;
+    canvas.width = this.state.width;
+    this.setState({
+      context: canvas.getContext("2d")
+    },()=>{
+      this.animate();
+    }); 
+  }
+
+  drawPlayer=()=>{
+    this.state.context.drawImage( 
+      this.image,
+      this.state.player.x, 
+      this.state.player.y,
+      this.state.player.width,
+      this.state.player.height, 
+    ); 
+  }
+
+  animate = () => {
+    requestAnimationFrame( this.animate );
+    this.state.context.clearRect(0,0, this.state.width, this.state.height);
+    this.drawPlayer();
+  }
+
   render() {
+    const style = {
+      background:'#000'
+    }
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <canvas ref="canvas" style={style} />
       </div>
     );
   }
